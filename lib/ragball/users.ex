9f9d1c77f6e1119e7eabd206f76068149365e570
Nicky Woolf <woolfnicky@gmail.com>
2018-05-28 07:07:39 -0700

    Some clean up

--- lib/ragball/users.ex
+++ lib/ragball/users.ex
@@ -2,13 +2,9 @@ defmodule Ragball.Users do
   alias Ragball.Repo
   alias Ragball.Users.User
 
-  def create_user_changeset(user, params \\ %{}) do
-    User.create_changeset(user, params)
-  end
-
   def create_user(params) do
     %User{}
-    |> create_user_changeset(params)
+    |> User.create_changeset(params)
     |> Repo.insert()
   end
 
--- lib/ragball/users/user.ex
+++ lib/ragball/users/user.ex
@@ -1,5 +1,6 @@
 defmodule Ragball.Users.User do
   use Ecto.Schema
+
   import Ecto.Changeset
   import RagballWeb.Gettext
 
